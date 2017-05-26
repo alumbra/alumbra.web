f59e298de2ba6d4e26f88f56b763b548cd26ab11
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-05-26 10:33:50 +0200

    add codox profile.

--- project.clj
+++ project.clj
@@ -9,4 +9,14 @@
                  [ring/ring-core "1.6.1"]
                  [hiccup "1.0.5"]
                  [cheshire "5.7.1"]]
+  :profiles {:codox
+             {:plugins [[lein-codox "0.10.3"]]
+              :dependencies [[codox-theme-rdash "0.1.2"]]
+              :codox {:project {:name "alumbra/web"}
+                      :metadata {:doc/format :markdown}
+                      :themes [:rdash]
+                      :source-uri "https://github.com/alumbra/alumbra.web/blob/v{version}/{filepath}#L{line}"
+                      :namespaces [alumbra.web.graphql-voyager
+                                   alumbra.web.graphiql-workspace]}}}
+  :aliases {"codox" ["with-profile" "+codox" "codox"]}
   :pedantic? :abort)
